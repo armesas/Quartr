81bd157ac1d9f6780479d285ad286810e956b15c
Andrew Armes <aarmes@ameronix.com>
2018-11-09 18:28:56 -0500

    Initial Commit

---
+++ Quartr/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quartr
+//
+//  Created by Andrew Armes on 11/9/18.
+//  Copyright © 2018 Ameronix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
